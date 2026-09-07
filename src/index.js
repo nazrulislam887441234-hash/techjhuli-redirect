6424e443307dacfa18634b9770022756dc3e95d4
nazrulislam <nazrulislam887441234@gmail.com>
2026-09-07 14:37:51 +0600

    Simplify fetch function to redirect to target URL

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
+  async fetch(request) {
+    const TARGET_URL = "https://www.ghotimarket.com";
 
-    // তোমার নতুন সাইট
-    const destinationBase = "https://www.ghotimarket.com";
-
-    // Option A: সবাইকে Homepage এ পাঠাবে (তুমি এটাই চেয়েছো)
-    const redirectUrl = `${destinationBase}/?utm_source=techjhuli&utm_medium=redirect`;
-
-    // Option B: যদি Path সহ পাঠাতে চাও, উপরের লাইন কমেন্ট করে নিচের 2 লাইন চালু করো
-    // const newUrl = new URL(destinationBase);
-    // newUrl.pathname = url.pathname;
-    // newUrl.search = url.search;
-    // const redirectUrl = newUrl.toString();
-
-    return new Response(null, {
-      status: 301,
-      headers: {
-        "Location": redirectUrl,
-        "Cache-Control": "public, max-age=86400",
-        "X-Robots-Tag": "noindex"
-      }
-    });
-  }
-}
+    return Response.redirect(TARGET_URL, 301);
+  },
+};
